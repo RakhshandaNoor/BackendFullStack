de2c51ff536f69d40db57579a544fa79ff71863c
RakhshandaNoor <rakhshanda2396@hotmail.com>
2018-10-30 11:05:27 +0530

    first commit

---
+++ src/main/java/com/stackroute/fullstackassignment/services/UserServiceImp.java
@@ -0,0 +1,31 @@
+package com.stackroute.fullstackassignment.services;
+
+
+import com.stackroute.fullstackassignment.domain.Users;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+    @Override
+    public Users addUser(Users user) {
+        return null;
+    }
+
+    @Override
+    public Users deleteUserById(Users user) {
+        return null;
+    }
+
+    @Override
+    public Users updateUser(Users user) {
+        return null;
+    }
+
+    @Override
+    public Users searchUserById(Long id) {
+        return null;
+    }
+
+}
